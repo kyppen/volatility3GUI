a16c03813df87ebf4b4ff32d4d35231d89972d05
fam <fagerhus.sondre@gmail.com>
2024-05-30 14:10:14 +0200

    something

--- GUiVolatility.py
+++ GUiVolatility.py
@@ -14,6 +14,7 @@ import openfile
 def run_volatility_command(command):
     # Run the Volatility command as a subprocess
     try:
+        print("trying to run ")
         # Example: subprocess.check_output(['vol.py', '-f', '/path/to/file', 'windows.pslist'])
         output = subprocess.check_output(command, shell=True, text=True)
     except subprocess.CalledProcessError as e:
@@ -36,14 +37,13 @@ output_text = tk.Text(root, bg="white")
 output_text.pack(side=tk.RIGHT, expand=True, fill=tk.BOTH)
 
 # Add buttons to the frame, each with a command to execute
-button1 = tk.Button(frame_buttons, text="open file",
+button4 = tk.Button(frame_buttons, text="open file",
                     command=lambda: openfile.browse_files)
-button1.pack(fill=tk.X)
 
+testttt = ['python3 volatility3/vol.py', '-f', 'MSEDGEWIN10-20231107-184623.raw', 'windows.pstree']
 button1 = tk.Button(frame_buttons, text="Command 1",
-                    command=lambda: run_volatility_command('volatility3/vol.py -f “/path/to/file” windows.pslist'))
+                    command=lambda: run_volatility_command(testttt))
 button1.pack(fill=tk.X)
-
 button2 = tk.Button(frame_buttons, text="Command 2",
                     command=lambda: run_volatility_command('volatility3/vol.py -f “/path/to/file” windows.psscan'))
 button2.pack(fill=tk.X)
